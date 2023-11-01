ffb87a8066ebaea824ea7500cb33f50a6137320d
Phnks Invictus <phnksinvictus@gmail.com>
2023-11-01 16:29:28 -0400

    revert data and new script

--- scripts/xml_id_regenerator.sh
+++ scripts/xml_id_regenerator.sh
@@ -2,23 +2,30 @@ xml_directory=".."
 
 declare -A id_map
 
-#for file in "$xml_directory"/Warhammer\ 40\,000.gst; do
-for file in "$xml_directory"/*.{gst,cat}; do
+for file in "$xml_directory"/Warhammer\ 40\,000.gst; do
+#for file in "$xml_directory"/*.{gst,cat}; do
 	echo $file
 	if [ -f "$file" ]; then
-		ids=($(grep -o "id=\"[^\"]*\"" "$file" | sed 's/id="//' | sed 's/"//'))
+		ids=($(grep -oiE "id=\"[^\"]*\"|Id=\"[^\"]*\"" "$file"))
 		for id in "${ids[@]}"; do
-			echo "OLD: "$id
-			if [[ -z "${id_map[$id]}" ]]; then
-				new_uuid=$(uuidgen)
-				id_map["$id"]=$new_uuid
-			else
-				new_uuid="${id_map[$id]}"
-			fi
-			echo "NEW: "$new_uuid
+		    echo "OLD: "$id
 
-		        sed -i "s/id=\"$id\"/id=\"$new_uuid\"/g" "$file"
-		done
+		    id_lower=$(echo "$id" | tr '[:upper:]' '[:lower:]')
+		    if [[ -z "${id_map[$id_lower]}" ]]; then
+                        new_uuid=$(uuidgen)
+                        id_map["$id_lower"]=$new_uuid
+                    else
+                        new_uuid="${id_map[$id_lower]}"
+                    fi
+
+	    	    echo "NEW: "$new_uuid
+
+		    if [[ $id == "id=\""* ]]; then
+			sed -i "s/$id/id=\"$new_uuid\"/g" "$file"
+		    else
+			sed -i "s/$id/Id=\"$new_uuid\"/g" "$file"
+		    fi		
+	    done
 	fi
 done
 
